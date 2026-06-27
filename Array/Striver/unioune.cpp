122577648db208d1111282b470e25062f9519981
priteshdev8767 <priteshpatil87679@gmail.com>
2026-06-28 01:40:29 +0530

    zala array paruant hot aahe kart rah

---
+++ Array/Striver/unioune.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+ 
+    set<int>st;
+
+    for(int i=0; i<n;i++)
+    {
+        int x; cin>>x;
+        st.insert(x);                                                                   
+    }
+    for(int i=0; i<m; i++)
+    {
+        int x; cin>>x;
+        st.insert(x);
+    }
+    for(auto it : st)
+    {
+        cout<<it<<" ";
+    }
+    return 0;
+}
+
+// for( auto   it  :  st )
+// //    |      |     |
+// //    |      |     container to loop through
+// //    |      variable that holds current element
+// //    auto-detect the data type
